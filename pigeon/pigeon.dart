df4c75e0d4d4ae59f5d3ac2cd1b96d5a78052bef
itheamc <itheamc@gmail.com>
2025-12-08 17:11:28 +0545

    pigeon setup

---
+++ pigeon/pigeon.dart
@@ -0,0 +1,73 @@
+import 'package:pigeon/pigeon.dart';
+
+@ConfigurePigeon(
+  PigeonOptions(
+    dartOut: 'lib/src/pigeon_generated.dart',
+    dartPackageName: 'paypal_payment',
+    dartOptions: DartOptions(),
+    kotlinOut:
+        'android/src/main/kotlin/com/itheamc/paypal_payment/pigeon/PigeonGenerated.kt',
+    kotlinOptions: KotlinOptions(),
+    swiftOut: 'ios/Classes/Pigeon/PigeonGenerated.swift',
+    swiftOptions: SwiftOptions(),
+  ),
+)
+@HostApi()
+abstract class PaypalPaymentHostApi {
+  void init(String clientId, String environment);
+}
+
+@HostApi()
+abstract class PayPalPaymentWebCheckoutHostApi {
+  void initiateCheckout(
+    String orderId,
+    String fallbackUrl,
+    String fundingSource,
+  );
+}
+
+// @FlutterApi()
+// abstract class PaypalPaymentFlutterApi {
+//   void onTest(double fps);
+// }
+
+sealed class PayPalWebCheckoutResultEvent {}
+
+class PayPalWebCheckoutSuccessResultEvent extends PayPalWebCheckoutResultEvent {
+  PayPalWebCheckoutSuccessResultEvent(this.orderId, this.payerId);
+
+  final String? orderId;
+  final String? payerId;
+}
+
+class PayPalWebCheckoutFailureResultEvent extends PayPalWebCheckoutResultEvent {
+  PayPalWebCheckoutFailureResultEvent(
+    this.orderId,
+    this.reason,
+    this.code,
+    this.correlationId,
+  );
+
+  final String? orderId;
+  final String reason;
+  final int code;
+  final String? correlationId;
+}
+
+class PayPalWebCheckoutCanceledResultEvent
+    extends PayPalWebCheckoutResultEvent {
+  PayPalWebCheckoutCanceledResultEvent(this.orderId);
+
+  final String? orderId;
+}
+
+class PayPalWebCheckoutErrorResultEvent extends PayPalWebCheckoutResultEvent {
+  PayPalWebCheckoutErrorResultEvent(this.error);
+
+  final String? error;
+}
+
+@EventChannelApi()
+abstract class PayPalWebCheckoutResultEventChannelApi {
+  PayPalWebCheckoutResultEvent payPalWebCheckoutResultEvent();
+}

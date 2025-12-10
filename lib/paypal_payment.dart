77971bd5851f503de48a96e845da51626db31cda
itheamc <itheamc@gmail.com>
2025-12-10 12:30:02 +0545

    exports public files

--- lib/paypal_payment.dart
+++ lib/paypal_payment.dart
@@ -1,8 +1,54 @@
+export 'src/paypal_payment.dart';
+export 'src/config/environment.dart';
 
-import 'paypal_payment_platform_interface.dart';
+export 'src/api/models/amount.dart';
+export 'src/api/orders/models/funding_source.dart';
+export 'src/api/orders/models/order_intent.dart';
+export 'src/api/orders/models/purchase_unit.dart';
+export 'src/api/orders/models/responses/order_create_response.dart';
+export 'src/api/orders/models/responses/capture_order_payment_response.dart'
+    hide
+        Payer,
+        PaymentSource,
+        Paypal,
+        PurchaseUnit,
+        Payments,
+        Capture,
+        SellerProtection,
+        SellerReceivableBreakdown,
+        Shipping,
+        Address;
 
-class PaypalPayment {
-  Future<String?> getPlatformVersion() {
-    return PaypalPaymentPlatform.instance.getPlatformVersion();
-  }
-}
+export 'src/api/payments/models/payment_intent.dart';
+export 'src/api/payments/v1/models/item.dart';
+export 'src/api/payments/v1/models/payer.dart';
+export 'src/api/payments/v1/models/payment_options.dart';
+export 'src/api/payments/v1/models/redirect_urls.dart';
+export 'src/api/payments/v1/models/shipping_address.dart';
+export 'src/api/payments/v1/models/transaction.dart';
+export 'src/api/payments/v1/models/responses/create_payment_response.dart';
+export 'src/api/payments/v1/models/responses/payment_details_response.dart';
+export 'src/api/payments/v1/models/responses/execute_payment_response.dart';
+export 'src/api/payments/v1/models/responses/capture_authorized_payment_response.dart';
+export 'src/api/payments/v1/models/responses/void_authorized_payment_response.dart';
+export 'src/api/payments/v1/models/responses/refund_captured_payment_response.dart';
+export 'src/api/payments/v1/models/responses/refund_sale_response.dart';
+
+export 'src/api/payments/v2/models/responses/capture_authorized_payment_response.dart'
+    hide SellerReceivableBreakdown, ExchangeRate, StatusDetails;
+export 'src/api/payments/v2/models/responses/void_authorized_payment_response.dart';
+export 'src/api/payments/v2/models/responses/refund_captured_payment_response.dart';
+
+export 'src/api/transactions/models/responses/transactions_list_response.dart'
+    hide
+        TaxAmount,
+        PayerInfo,
+        PayerName,
+        TransactionDetail,
+        AuctionInfo,
+        CartInfo,
+        ItemDetail,
+        EInfo,
+        ShippingInfo,
+        Address,
+        TransactionInfo;
